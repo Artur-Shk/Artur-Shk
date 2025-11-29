3117e6af46794d103bcddccf4f5e353ee2c59dd5
artur <a.s.shkurat@gmail.com>
2025-11-29 14:51:59 +0200

    Sorted by folders

--- files/files_practice.py
+++ files/files_practice.py
@@ -2,11 +2,11 @@ import csv
 '''Збережіть рядок 'Test variable to write to file' у змінну test1 і запишіть змінну test1 у файл з ім’ям tests.txt.
 Відкрийте файл tests.txt і зчитайте його вміст у рядок test2. Порівняйте рядки test1 і test2?'''
 test1 = "Test variable to write to file"
-with open("../tests.txt", "w", encoding="utf-8") as f:
+with open("tests.txt", "w", encoding="utf-8") as f:
     f.write(test1)
     f.write(test1)
 
-with open("../tests.txt", "r", encoding="utf-8") as f:
+with open("tests.txt", "r", encoding="utf-8") as f:
     test2 = f.readline().rstrip("\n")
 if test1 == test2:
     print("C")
